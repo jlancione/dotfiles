1f261641c3055e8ffa2797d1dc6e663c0da6d468
jlancione <jacopo.lancione@gmail.com>
2024-03-02 18:00:29 +0100

    Add alacritty and fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,6 @@
+
+fish_add_path /opt/homebrew/bin
+fish_add_path /Library/TeX/texbin
+
+alias skim='/Applications/Skim.app/Contents/MacOS/Skim'
+alias config='/opt/homebrew/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
