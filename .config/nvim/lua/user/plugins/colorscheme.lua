2130954e142b785e2256c227f448988d1b36e99d
jlancione <jacopo.lancione@gmail.com>
2024-05-01 11:21:24 +0200

    remove hold shift to go to end line, toggle colorscheme in nvim

--- .config/nvim/lua/user/plugins/colorscheme.lua
+++ .config/nvim/lua/user/plugins/colorscheme.lua
@@ -7,14 +7,14 @@ return { -- gruvbox, sonokai, just for inspiration…
     require('onedark').setup  {
       -- Main options --
       style = 'deep', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-      transparent = false,  -- Show/hide background
+      transparent = false,  -- Show/hide background (perdi il vantaggio di avere un colorscheme dark e uno light)
       term_colors = true, -- Change terminal color as per the selected theme style
       ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
       cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
 
       -- toggle theme style ---
-      toggle_style_key = nil, -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
-      toggle_style_list = {'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'}, -- List of styles to toggle between
+      toggle_style_key = "<leader>ts", -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+      toggle_style_list = {'deep', 'light'}, -- {'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'}, -- List of styles to toggle between
 
       -- Change code style ---
       -- Options are italic, bold, underline, none
