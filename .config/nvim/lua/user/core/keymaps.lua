66175e8aafa4ad96d0c8bfd4f6e8c191931cd67c
jlancione <jacopo.lancione@gmail.com>
2025-02-07 18:10:13 +0100

    Available modifiers as comment

--- .config/nvim/lua/user/core/keymaps.lua
+++ .config/nvim/lua/user/core/keymaps.lua
@@ -1,6 +1,6 @@
 local opts = { noremap = true, silent = true }
 
-local term_opts = { silent = true }
+-- local term_opts = { silent = true }
 
 -- Shorten function name
 local keymap = vim.keymap.set
@@ -18,7 +18,18 @@ vim.g.maplocalleader = " "
 --   term_mode = "t",
 --   command_mode = "c",
 
+-- :h keycodes
+-- C = control
+-- S = shift
+-- A = alt key, same as M (meta key)
+-- D = gui key, command
+-- leader = space (custom)
+--
 -- Normal --
+-- Better moving through file
+keymap("n", "<C-d>", "<C-d>zz", opts)
+keymap("n", "<C-u>", "<C-u>zz", opts)
+
 -- Better window navigation
 keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
@@ -26,18 +37,18 @@ keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
 -- Resize with arrows
-keymap("n", "<C-Up>", ":resize -2<CR>", opts)
-keymap("n", "<C-Down>", ":resize +2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+keymap("n", "<S-Up>", ":resize -2<CR>", opts)
+keymap("n", "<S-Down>", ":resize +2<CR>", opts)
+keymap("n", "<S-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<S-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
 -- Move text up and down
-keymap("n", "<A-j>", ":m .+1<CR>==", opts)
-keymap("n", "<A-k>", ":m .-2<CR>==", opts)
+keymap("n", "<D-j>", ":m .+1<CR>==", opts)
+keymap("n", "<D-k>", ":m .-2<CR>==", opts)
 
 -- Kill search highlights
 keymap("n", "<CR>", ":noh<CR>", opts)
@@ -47,8 +58,8 @@ keymap("n", "<", "<S-v><<esc>", opts)
 keymap("n", ">", "<S-v>><esc>", opts)
 
 -- Navigate display lines
-keymap("n", "J", "gj", opts)
-keymap("n", "K", "gk", opts)
+-- keymap("n", "J", "gj", opts)
+-- keymap("n", "K", "gk", opts)
 
 -- Insert --
 -- Press jk fast to exit insert mode 
@@ -57,24 +68,22 @@ keymap("i", "kj", "<ESC>", opts)
 
 -- Visual --
 -- Navigate display lines
-keymap("v", "J", "gj", opts)
-keymap("v", "K", "gk", opts)
+-- keymap("v", "J", "gj", opts)
+-- keymap("v", "K", "gk", opts)
 
 -- Stay in indent mode
 keymap("v", "<", "<gv^", opts)
 keymap("v", ">", ">gv^", opts)
 
 -- Move text up and down
-keymap("v", "<A-j>", ":m '>+1<CR>gv=gv", opts)
-keymap("v", "<A-k>", ":m '<-2<CR>gv=gv", opts)
+keymap("v", "<D-j>", ":m '>+1<CR>gv=gv", opts)
+keymap("v", "<D-k>", ":m '<-2<CR>gv=gv", opts)
 keymap("v", "p", '"_dP', opts)
 
 -- Visual Block --
 -- Move text up and down
-keymap("x", "J", ":m '>+1<CR>gv=gv", opts)
-keymap("x", "K", ":m '<-2<CR>gv=gv", opts)
-keymap("x", "<A-j>", ":m '>+1<CR>gv=gv", opts)
-keymap("x", "<A-k>", ":m '<-2<CR>gv=gv", opts)
+keymap("x", "<D-j>", ":m '>+1<CR>gv=gv", opts)
+keymap("x", "<D-k>", ":m '<-2<CR>gv=gv", opts)
 
 -- Terminal --
 -- Better terminal navigation
@@ -82,11 +91,11 @@ keymap("x", "<A-k>", ":m '<-2<CR>gv=gv", opts)
 -- keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
 -- keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 -- keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
-keymap("n", "<c-s-\\>", "<cmd>ToggleTerm direction=vertical size=60<cr>", opts)
+keymap("n", "<C-S-\\>", "<cmd>ToggleTerm direction=vertical size=60<cr>", opts)
 
 keymap("n", "<leader>f", "<cmd>Telescope find_files<cr>", opts)
 -- keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
-keymap("n", "<c-t>", "<cmd>Telescope live_grep<cr>", opts)
+keymap("n", "<C-t>", "<cmd>Telescope live_grep<cr>", opts)
 
 -- Nvimtree
 keymap("n", "<leader>e", "<cmd>NvimTreeToggle<cr>", opts)
