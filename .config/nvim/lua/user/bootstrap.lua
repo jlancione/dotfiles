2b34c3789c092de1ef83e8e69f581804f3620739
jlancione <jacopo.lancione@gmail.com>
2025-02-07 15:51:43 +0100

    Minors

--- .config/nvim/lua/user/bootstrap.lua
+++ .config/nvim/lua/user/bootstrap.lua
@@ -34,7 +34,6 @@ require("lazy").setup({
 
 -- TODOs 
 -- sarebbe carino usare 1 pcall 
--- ed era comodo l'autocommand che ricaricava nvim ogni volta che scrivevo su plugins.lua (ma avendo tutto spezzettato nn so se sia possibile…
 -- e poi si può chiaramente dare 1 occhiata alla documentazione di lazy.nvim
 -- Use a protected call so we don't error out on first use
 -- local status_ok, packer = pcall(require, "packer")
