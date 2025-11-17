6aee8a03cfee1e3d67ce23b8ec5d35202a1b59c5
jlancione <jacopo.lancione@gmail.com>
2025-11-17 18:09:08 +0100

    Minor:Remove useless comments

--- .config/nvim/lua/jl/plugins/wiki.lua
+++ .config/nvim/lua/jl/plugins/wiki.lua
@@ -4,13 +4,6 @@ return {
     init = function()  -- not `config` because it needs to run before loading the plugin
 
       vim.g.wiki_root = "~/wiki"
-      -- alla fine si tratta di configurare dle vim.g.wiki_something
-      -- g.wiki_global_load    vogliamo tenere solo root?
-      -- g.wiki_select_method  puoi usare Telescope
-      -- g.wiki_index_name
-      -- g.wiki_journal
-      -- c'è un sistema di tags che sicuramente è utile
-      -- si possono specificare dei templates
 
     end
   }
