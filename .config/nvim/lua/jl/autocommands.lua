81c1c209b74b8dc21b0dca68322eaa133c2baac2
jlancione <jacopo.lancione@gmail.com>
2025-11-18 07:50:32 +0100

    Fix:cd wiki root won't clash with 'nvim dir/'

--- .config/nvim/lua/jl/autocommands.lua
+++ .config/nvim/lua/jl/autocommands.lua
@@ -1,10 +1,9 @@
-local group_cdpwd = vim.api.nvim_create_augroup("group_cdpwd", { clear = true })
+local group_wiki = vim.api.nvim_create_augroup("group_wiki", { clear = true })
 
 -- Autocommands --
 -- They run each time a condition is met. See :help lua-guide-autocommands
-vim.api.nvim_create_autocmd("VimEnter", {
-  group = group_cdpwd,
-  pattern = "*",
+vim.api.nvim_create_autocmd("User", {
+  pattern = "WikiBufferInitialized",
   callback = function()
     vim.api.nvim_set_current_dir(vim.fn.expand("%:p:h"))    
     -- % Current file name, :p expand to full path, :h last path component removed (head)
