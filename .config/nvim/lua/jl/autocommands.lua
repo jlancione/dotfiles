1a507bb6cd8c36d4e022151fa0443e572d2832ae
jlancione <jacopo.lancione@gmail.com>
2025-11-18 07:56:36 +0100

    Minor:decluttering

--- .config/nvim/lua/jl/autocommands.lua
+++ .config/nvim/lua/jl/autocommands.lua
@@ -1,9 +1,8 @@
-local group_wiki = vim.api.nvim_create_augroup("group_wiki", { clear = true })
-
 -- Autocommands --
 -- They run each time a condition is met. See :help lua-guide-autocommands
+
 vim.api.nvim_create_autocmd("User", {
-  pattern = "WikiBufferInitialized",
+  pattern = "WikiBufferInitialized",  -- wiki.vim User event
   callback = function()
     vim.api.nvim_set_current_dir(vim.fn.expand("%:p:h"))    
     -- % Current file name, :p expand to full path, :h last path component removed (head)
