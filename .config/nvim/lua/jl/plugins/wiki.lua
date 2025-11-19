a7948cd29b2e2c5ef732576a68a08207245843e0
jlancione <jacopo.lancione@gmail.com>
2025-11-19 08:29:31 +0100

    Feat:use Telescope for wiki UI search

--- .config/nvim/lua/jl/plugins/wiki.lua
+++ .config/nvim/lua/jl/plugins/wiki.lua
@@ -5,6 +5,20 @@ return {
 
       vim.g.wiki_root = "~/wiki"
 
+      local telescope = require("wiki.telescope")
+      -- Set search UI
+      vim.g.wiki_select_method = {
+        pages = telescope.pages,  -- for WikiPages
+        -- tags  = telescope.tags,
+        toc   = telescope.toc,    -- for WikiToc
+        links = telescope.links,  -- for WikiLinkAdd
+      }
+
+      local keymap = vim.keymap.set
+      -- keymaps
+      keymap("n", "<leader>la", ":WikiLinkAdd<CR>", { desc = "[L]ink [A]dd" }) 
+      -- keymap("n", "<leader>wp", ":WikiPages<CR>", { desc = "Search [W]iki [P]ages" }) 
+
     end
   }
 }
