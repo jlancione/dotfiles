326e8349cc05f70e75526fe10e39530c179bbc34
jlancione <jacopo.lancione@gmail.com>
2026-01-22 15:46:03 +0100

    Add markdown

---
+++ .config/nvim/lua/jl/plugins/markdown.lua
@@ -0,0 +1,24 @@
+-- https://github.com/MeanderingProgrammer/render-markdown.nvim/wiki
+return {
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.icons" },
+    ft = "markdown",
+    opts = {
+      enabled = true,
+      render_modes = { "n", "c", "v", "V", "\22", "no", "R", },
+      file_types = { "markdown" },
+      -- latex = {
+      --      enabled = true,
+      --      converter = "latex2text",
+      --  },
+      heading = { position = "inline", backgrounds = {}, },
+      bullet = { enabled = false },
+      checkbox = {
+          checked = { icon = "" },
+          custom = { todo = { raw = "[-]", rendered = "" } }, -- , highlight = "RenderMarkdownTodo", scope_highlight = nil }
+      },
+      -- link = { render_modes = { "n", "c", "v" } },
+    }
+  }
+}
