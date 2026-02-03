7516ab2acd568de9793b57df24a5ca7b62d0fd04
jlancione <jacopo.lancione@gmail.com>
2026-02-04 00:09:48 +0100

    Add Spell keybinding

--- .config/nvim/lua/jl/keymaps.lua
+++ .config/nvim/lua/jl/keymaps.lua
@@ -60,10 +60,12 @@ keymap( "n", "<leader>st", function()
   job_id = vim.bo.channel
   vim.fn.chansend(job_id, { "fish_default_key_bindings && clear\r" }) -- \r stands for <CR>
   vim.cmd.wincmd("J") -- places the window at the bottom
-  vim.api.nvim_win_set_height(0,13)
+  vim.api.nvim_win_set_height(0,8)
 end,
   "Open [S]mall [T]erminal" )
 keymap( "t", "<Esc>", "<C-Bslash><C-N>", "[Esc]ape insert mode in terminal" )
+
+keymap( "n", "<leader>sp", ":setlocal spell spelllang=en_gb<CR>:echo 'Spell ON'<CR>", "Launch [SP]ell" )
 
 
 -- Remove clutter from insert mode
