64cf7edc035522cf7ae79dd323065b62b592156e
jlancione <jacopo.lancione@gmail.com>
2026-03-25 09:37:58 +0100

    Add pyright and lua_ls, suppress undefined global vim

--- .config/nvim/lua/jl/plugins/lsp.lua
+++ .config/nvim/lua/jl/plugins/lsp.lua
@@ -2,10 +2,20 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
+
+      vim.lsp.enable("lua_ls")
       vim.lsp.enable("texlab")
       vim.lsp.enable("clangd")
+      vim.lsp.enable("basedpyright")
       -- vim.lsp.set_log_level("debug")
       vim.diagnostic.config({ virtual_text = true })
+
+      vim.lsp.config("lua_ls", {  -- suppress warning Undefined global `vim`
+        settings = {
+          Lua = {
+            diagnostics = { globals = { "vim" } }
+          }}
+      })
     end,
   }
 }
