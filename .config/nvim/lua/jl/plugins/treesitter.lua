a8f398b9568185a986e91bb7a96c23788ca3a613
jlancione <jacopo.lancione@gmail.com>
2026-04-15 15:28:24 +0200

    Treesitter switch to branch main

--- .config/nvim/lua/jl/plugins/treesitter.lua
+++ .config/nvim/lua/jl/plugins/treesitter.lua
@@ -1,25 +1,12 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    branch = "master",
     lazy = false,
-    build = ":TSUpdate",  -- to automatically update the parsers to the version of TS
+    build = ":TSUpdate",
     config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = { "cpp", "lua", "python", "markdown" },
-        -- bash, cmake, csv, fish, gitignore, gnuplot, markdown-inline, json, latex, make, tcl, typst, vim
-        -- ignore_installed = {}, -- DEBUG
-
-        -- Modules --
-        highlight = {
-          enabled = true,
-          -- disable = {} -- DEBUG
-        },
-        indent = {        -- for = operator
-          enable = true,
-          -- disable = {}, -- DEBUG
-        },
-      })
+      require('nvim-treesitter').install {
+        "lua", "cpp", "python", "latex", "markdown", "markdown_inline", "bash", "fish", "gitignore",
+      }
     end
   }
 }
